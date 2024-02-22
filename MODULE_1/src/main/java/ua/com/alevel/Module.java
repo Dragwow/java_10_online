7e4be7f16a12c1721166b4ba580d5d7ab257022c
Admin <pokotylomain@gmail.com>
2024-02-22 09:55:21 +0200

    homework

---
+++ MODULE_1/src/main/java/ua/com/alevel/Module.java
@@ -0,0 +1,11 @@
+package ua.com.alevel;
+
+import ua.com.alevel.console.FirstPage;
+
+import java.io.IOException;
+
+public class Module {
+    public static void main(String[] args) throws IOException {
+        new FirstPage().start();
+    }
+}
