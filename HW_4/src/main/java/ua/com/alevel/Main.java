3db227a6b23f12deb70eca1880f190b4f07d4bbd
Admin <pokotylomain@gmail.com>
2024-05-20 21:46:59 +0300

    homework

---
+++ HW_4/src/main/java/ua/com/alevel/Main.java
@@ -0,0 +1,44 @@
+package ua.com.alevel;
+
+
+import java.io.IOException;
+import java.util.Arrays;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        MatList<Number> array1 = new MatList<>();
+//        MatList<Number> array2 = new MatList<>(
+//                new Number[]{1,2,3,4,5},
+//                new Number[]{1090},
+//                new Number[]{122,212,20}
+//                );
+//        array1.add(1);
+//        array1.add(22);
+//        MatList<Number> array3 = new MatList<>(array1,array2);
+//        MatList<Number> array4 = new MatList<>();
+//        array3.join(array4);
+//        System.out.println(Arrays.toString(array1.toArray()));
+//        System.out.println(Arrays.toString(array2.toArray()));
+//        System.out.println(Arrays.toString(array3.toArray()));
+//        array4.intersection(array1);
+//        System.out.println(Arrays.toString(array4.toArray()));
+//        array1.sortDesc(4);
+//        array2.sortAsc();
+//        array3.sortDesc(1,2);
+//        System.out.println(Arrays.toString(array1.toArray()));
+//        System.out.println(Arrays.toString(array2.toArray()));
+//        System.out.println(Arrays.toString(array3.toArray()));
+//        System.out.println(array1.get(1));
+//        System.out.println(array2.getMax());
+//        System.out.println(array2.getMin());
+//        System.out.println(array1.getAverage());
+//        System.out.println(array3.getMedian());
+//        array3.clear();
+//        System.out.println(Arrays.toString(array3.toArray()));
+//        array2.cut(1,2);
+//        System.out.println(Arrays.toString(array2.toArray()));
+    }
+
+}
+
--- HW_4/src/main/java/ua/com/alevel/MatList.java
+++ HW_4/src/main/java/ua/com/alevel/MatList.java
@@ -7,13 +7,19 @@ public class MatList<E extends Number> {
     private int size;
 
     public MatList(){
-
+        elements = (E[]) new Number[10];
+        size = 0;
     }
     public MatList(E[] ... numbers){
-
+        this();
+        for (E[] number : numbers){
+            add(number);
+        }
     }
 
     public MatList(MatList ... numbers){
+        this();
+        join(numbers);
 
     }
 
@@ -34,11 +40,8 @@ public class MatList<E extends Number> {
 
     public void join(MatList<E> ... ml){
         for (MatList<E> list : ml) {
-            add(list.toArray());
+            add((E[]) list.toArray());
         }
-    }
-    public E[] toArray() {
-        return Arrays.copyOf(elements, size);
     }
 
     public void intersection (MatList ... ml){
@@ -54,16 +57,202 @@ public class MatList<E extends Number> {
     }
 
     public void sortDesc(){
-
+        bubbleSorter(elements, size);
     }
 
-    private void bubbleSorter(){
+    public void sortDesc(int firstIndex, int lastIndex){
+        for (int i = firstIndex; i < lastIndex; i++) {
+            for (int j = firstIndex + 1; j < (lastIndex - i); j++) {
+                if (elements[j - 1 ].doubleValue() < elements[j].doubleValue()){
+                    E temp = elements[j];
+                    elements[j] = elements[j - 1];
+                    elements[j - 1] = temp;
+                }
 
+            }
+
+        }
     }
 
-    public E[] toArray(int firstIndex, int lastIndex) {
-        return Arrays.copyOfRange(elements, firstIndex, lastIndex + 1);
+    public void sortDesc(E value){
+        int startIndex = -1;
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(value)){
+                startIndex = i;
+                break;
+            }
+        }
+        if (startIndex != -1){
+            sortDesc();
+        }
     }
+
+    public void sortAsc(){
+        bubbleSorterAsc(elements, size);
+    }
+
+    public void sortAsc(int firstIndex, int lastIndex){
+        for (int i = firstIndex; i < lastIndex; i++) {
+            for (int j = firstIndex + 1; j < (lastIndex - i); j++) {
+                if (elements[j - 1 ].doubleValue() < elements[j].doubleValue()){
+                    E temp = elements[j];
+                    elements[j] = elements[j - 1];
+                    elements[j - 1] = temp;
+                }
+            }
+        }
+    }
+
+    public void sortAsc(E value){
+        int startIndex = -1;
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(value)){
+                startIndex = i;
+                break;
+            }
+        }
+        if (startIndex != -1){
+            sortAsc();
+        }
+    }
+
+    public Number get(int index){
+        return elements[index];
+
+    }
+    public Number getMax(){
+        Number[] array = Arrays.copyOf(elements, size);
+        bubbleSorter(elements, size);
+        return array[0];
+    }
+
+    public Number getMin(){
+        Number[] array = Arrays.copyOf(elements, size);
+        bubbleSorterAsc(elements, size);
+        return array[0];
+    }
+
+    public Number getAverage(){
+        double sum = 0.0;
+        for (int i = 0; i < size; i++) {
+            sum += elements[i].doubleValue();
+        }
+        return sum / size;
+    }
+
+    public Number getMedian(){
+        if(size == 0){
+            return  null;
+        }
+        Number[] array = Arrays.copyOf(elements, size);
+        bubbleSorter(array, this.size);
+        double median;
+        if (size % 2 == 0){
+            median = (array[size / 2].doubleValue() + array[size / 2 -1].doubleValue()) / 2;
+        }
+        else {
+            median = array[size / 2].doubleValue();
+        }
+        return median;
+    }
+
+    public Number[] toArray() {
+        Number[] array = new Number[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = elements[i];
+        }
+        return array;
+    }
+
+    public Number[] toArray(int firstIndex, int lastIndex){
+        if (firstIndex < 0 || firstIndex >= size || lastIndex < 0 || lastIndex >= size || firstIndex > lastIndex) {
+            return new Number[0];
+        }
+        int newArraySize = lastIndex - firstIndex + 1;
+        Number[] array = new Number[newArraySize];
+        for (int i = 0; i < newArraySize; i++) {
+            array[i] = elements[firstIndex + i];
+        }
+        return array;
+    }
+
+    public MatList<E> cut(int firstIndex, int lastIndex){
+        if (firstIndex < 0 || firstIndex >= size || lastIndex < 0 || lastIndex >= size || firstIndex > lastIndex) {
+            return new MatList<>();
+        }
+        int newArraySize = firstIndex - lastIndex + 1;
+        E[] newElements = (E[]) new Number[newArraySize];
+        for (int i = 0; i < newArraySize; i++) {
+            newElements[i] = elements[firstIndex + i];
+        }
+        MatList<E> newList = new MatList<>();
+        newList.elements = newElements;
+        newList.size = newArraySize;
+        return  newList;
+    }
+
+    public void clear(){
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    public void clear(Number[] numbers){
+        if (numbers == null || numbers.length == 0){
+            return;
+        }
+        int newSize = 0;
+        E[] newElements = (E[]) new Number[elements.length];
+        for (int i = 0; i < size; i++) {
+            boolean found = false;
+            for (Number number : numbers) {
+                if (elements[i].equals(number)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                newElements[newSize++] = elements[i];
+            }
+        }
+        elements = newElements;
+        size = newSize;
+    }
+
+
+    private void bubbleSorter(Number[] sortArr, int size){
+        int n = size;
+        Number temp;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (sortArr[j - 1].doubleValue() < sortArr[j].doubleValue()){
+                    temp = sortArr[j - 1];
+                    sortArr[j - 1] = sortArr[j];
+                    sortArr[j - 1] = temp;
+                }
+
+            }
+
+        }
+    }
+
+    private void bubbleSorterAsc(Number[] sortArr, int size){
+        int n = size;
+        Number temp;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (sortArr[j - 1].doubleValue() < sortArr[j].doubleValue()){
+                    temp = sortArr[j - 1];
+                    sortArr[j - 1] = sortArr[j];
+                    sortArr[j - 1] = temp;
+                }
+
+            }
+
+        }
+    }
+
     private void createArray(){
         E[] newArray =(E[])new Number[elements.length + 10];
         System.arraycopy(elements, 0, newArray, 0, elements.length);
